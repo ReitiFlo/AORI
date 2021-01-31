61a1ed63754b3a392082b3d5e1b89380bb435c5c
Florian Reitmair <florian.reitmair@gmail.com>
2021-01-31 17:58:08 +0100

    Insert Download class and redefine

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,17 +1,22 @@
-import pandas as pd
-import requests
 import gdown
 
-from io import StringIO
+
+class Downloader:
+	def __init__(self, input_url, output_name):
+		self.input_url = input_url
+		self.output_name = output_name
+
+	def download_from_gdrive(self):
+		gdown.download(self.input_url, self.output_name, quiet=False)
 
 
 
-url = 'https://drive.google.com/uc?id=15Xfy-uf1d3SmtK7b5oTGKuCFfap3Anuv'
-output = 'renewable_power_plants_DE.csv'
-gdown.download(url, output, quiet=False)
+#url = 'https://drive.google.com/uc?id=15Xfy-uf1d3SmtK7b5oTGKuCFfap3Anuv'
+#output = 'renewable_power_plants_DE.csv'
+#gdown.download(url, output, quiet=False)
 
-md5 = '567703f6a41ee9a9587862053c63fe7e'
-gdown.cached_download(url, output, md5=None, postprocess=gdown.extractall)
+#md5 = '567703f6a41ee9a9587862053c63fe7e'
+#gdown.cached_download(url, output, md5=None, postprocess=gdown.extractall)
 
 #orig_url='https://drive.google.com/file/d/15Xfy-uf1d3SmtK7b5oTGKuCFfap3Anuv/view?usp=sharing'
 
